2d08378f3d3e6bf084ce517686ec883eff668188
RomanKuzmich <roma_kuzmich_03@mail.ru>
2025-11-24 20:22:05 +0300

    feat: add initial migration

--- app/config.py
+++ app/config.py
@@ -6,13 +6,13 @@ class Settings(BaseSettings):
     DB_HOST: str | None = None
     DB_PORT: int | None = None
     DB_USER: str | None = None
-    DB_PASSWORD: str | None = None
+    DB_PASS: str | None = None
     DB_NAME: str | None = None
 
     @computed_field
     @property
     def database_url(self) -> str:
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@ {self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(
         env_file=".env.sample",
